38acb30a0220a5c65cb7ed4529c4bbca1d53e487
Sadullo <sadullo@gmail.com>
2023-11-04 09:56:17 +0500

    feat: add home ui

--- src/components/HeaderCart.tsx
+++ src/components/HeaderCart.tsx
@@ -1,7 +1,7 @@
 export default function HeaderCart() {
   return (
     <div className="header__cart">
-      <a href="/cart.html" className="button button--cart">
+      <a href="/cart" className="button button--cart">
         <span>520 ₽</span>
         <div className="button__delimiter"></div>
         {cartLogo()}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,24 @@
+import Categories from '../components/Categories';
+import PizzaBlock from '../components/PizzaBlock';
+import Sort from '../components/Sort';
+
 export default function Home() {
-  return <h2>Home page</h2>;
+  return (
+    <div className="content">
+      <div className="container">
+        <div className="content__top">
+          <Categories />
+          <Sort />
+        </div>
+        <h2 className="content__title">Все пиццы</h2>
+        <div className="content__items">
+          <PizzaBlock />
+          <PizzaBlock />
+          <PizzaBlock />
+          <PizzaBlock />
+          <PizzaBlock />
+        </div>
+      </div>
+    </div>
+  );
 }
