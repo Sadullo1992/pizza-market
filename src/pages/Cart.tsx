11266eae7f6d1a7623ccdd7e71455cf14b1b80a5
Sadullo <sadullo@gmail.com>
2023-11-09 00:47:04 +0500

    feat: add empty cart component

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -2,9 +2,10 @@ import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import Button from '../components/Button';
 import CartItem from '../components/CartItem';
+import EmptyCart from '../components/EmptyCart';
 import { CartContext } from '../context/CartContext';
 import { TCartContext } from '../types/types';
-import { CartIcon, TrashIcon } from '../utils';
+import { BackIcon, CartIcon, TrashIcon } from '../utils';
 
 export default function Cart() {
   const { cartItems, totalCount, totalPrice, onClearCart } = useContext<TCartContext>(CartContext);
@@ -14,59 +15,49 @@ export default function Cart() {
   return (
     <div className="content">
       <div className="container container--cart">
-        <div className="cart">
-          <div className="cart__top">
-            <h2 className="content__title">
-              <CartIcon />
-              Корзина
-            </h2>
-            <div className="cart__clear" onClick={() => onClearCart()}>
-              <TrashIcon />
-              <span>Очистить корзину</span>
+        {totalCount > 0 ? (
+          <div className="cart">
+            <div className="cart__top">
+              <h2 className="content__title">
+                <CartIcon />
+                Корзина
+              </h2>
+              <div className="cart__clear" onClick={() => onClearCart()}>
+                <TrashIcon />
+                <span>Очистить корзину</span>
+              </div>
             </div>
-          </div>
-          <div className="content__items">
-            {cartPizzas.map((obj) => (
-              <CartItem key={obj.id} item={obj} />
-            ))}
-          </div>
-          <div className="cart__bottom">
-            <div className="cart__bottom-details">
-              <span>
-                Всего пицц: <b>{totalCount} шт.</b>
-              </span>
-              <span>
-                Сумма заказа: <b>{totalPrice} ₽</b>
-              </span>
+            <div className="content__items">
+              {cartPizzas.map((obj) => (
+                <CartItem key={obj.id} item={obj} />
+              ))}
             </div>
-            <div className="cart__bottom-buttons">
-              <Link to={'/'}>
-                <Button className="button--outline button--add go-back-btn">
-                  {backIcon()}
-                  <span>Вернуться назад</span>
+            <div className="cart__bottom">
+              <div className="cart__bottom-details">
+                <span>
+                  Всего пицц: <b>{totalCount} шт.</b>
+                </span>
+                <span>
+                  Сумма заказа: <b>{totalPrice} ₽</b>
+                </span>
+              </div>
+              <div className="cart__bottom-buttons">
+                <Link to={'/'}>
+                  <Button className="button--outline button--add go-back-btn">
+                    <BackIcon />
+                    <span>Вернуться назад</span>
+                  </Button>
+                </Link>
+                <Button className="pay-btn">
+                  <span>Оплатить сейчас</span>
                 </Button>
-              </Link>
-              <Button className="pay-btn">
-                <span>Оплатить сейчас</span>
-              </Button>
+              </div>
             </div>
           </div>
-        </div>
+        ) : (
+          <EmptyCart />
+        )}
       </div>
     </div>
-  );
-}
-
-function backIcon() {
-  return (
-    <svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path
-        d="M7 13L1 6.93015L6.86175 1"
-        stroke="#D3D3D3"
-        strokeWidth="1.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-    </svg>
   );
 }
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,5 +2,6 @@ import PlusIcon from './PlusIcon';
 import MinusIcon from './MinusIcon';
 import CartIcon from './CartIcon';
 import TrashIcon from './TrashIcon';
+import BackIcon from './BackIcon';
 
-export { PlusIcon, MinusIcon, CartIcon, TrashIcon };
+export { PlusIcon, MinusIcon, CartIcon, TrashIcon, BackIcon };
